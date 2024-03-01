3c5e15acf05d65e2ca81202742608f796734316e
AuliaVirza101 <serjekenterprise@gmail.com>
2024-03-01 00:35:24 -0800

    first commit

---
+++ database/migrations/2024_03_01_070620_create_m_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('m_user', function (Blueprint $table) {
+            $table->id('user_id');
+            $table->unsignedBIgInteger('level_id')->index();//indexing untuk foreignkey
+            $table->String('nama',100);
+            $table->String('password');
+            $table->timestamps();
+
+            //mendefinisikan foreign key pada kolom level id mengacu pada kolom level id di tabel m_level
+            $table->foreign('level_id')->references('level_id')->on('m_level');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('m_user');
+    }
+};
