7c3f8f0c75e27c1872d013ccacd1e00810899cdb
AuliaVirza101 <serjekenterprise@gmail.com>
2024-05-09 03:43:41 -0700

    practicum 2 js 10

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\controllers\Api\RegisterController;
+use App\Http\Controllers\Api\LoginController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,3 +19,7 @@ use App\Http\controllers\Api\RegisterController;
 //     return $request->user();
 // });
 Route::post('/register', RegisterController::class)->name('register');
+Route::post('/login', LoginController::class)->name('Login');
+Route::middleware('auth:api')->get('/user', function (Request $request){
+    return $request->user();
+});
